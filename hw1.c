7187a8a85e8012e186554e1e93cc61bd59396ec8
Stanley910915 <stanley910915@gmail.com>
2025-11-29 15:24:15 +0800

    Add homework files

---
+++ hw1.c
@@ -0,0 +1,7 @@
+#include "stdio.h" // 為了 printf
+
+void run_my_hw1(void) 
+{
+    printf("Hello from hw1.c!\n");
+    printf("My HW1 program is running.\n");
+}
---
+++ hw1.h
@@ -0,0 +1,6 @@
+#ifndef HW1_H
+#define HW1_H
+
+void run_my_hw1(void);
+
+#endif
